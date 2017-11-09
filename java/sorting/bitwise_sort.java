18a585aa97199f680038645e4db269a25abd1a7b
Darshan Patel <darshanpatel25894@gmail.com>
2017-11-09 14:53:58 -0500

    (Sorting) Added changes to Bitwise Sort problem

--- java/sorting/bitwise_sort.java
+++ java/sorting/bitwise_sort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class bitwise_sort {
 
     public static void main(String[] args) {
-        int[] bits = {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1};
+        int[] bits = {0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1};
         bits = sort(bits);
 
         System.out.println(Arrays.toString(bits));
@@ -14,19 +14,14 @@ public class bitwise_sort {
     public static int[] sort(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
-        int index = 0;
 
         while(start < end) {
-            if(arr[index] == 0 && index > start) {
-                arr[index] = arr[start];
-                arr[start] = 0;
-                start++;
-            } else if(arr[index] == 1 && index < end) {
-                arr[index] = arr[end];
+            if(arr[start] != 0) {
+                arr[start] = arr[end];
                 arr[end] = 1;
                 end--;
             } else {
-                index++;
+                start++;
             }
 
         }
