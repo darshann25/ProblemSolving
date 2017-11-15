d439c1cc0d5e413183261e35b23801d962e0e327
Darshan Patel <darshanpatel25894@gmail.com>
2017-11-14 20:14:41 -0500

    (WarmUp) Added sample code for getting used to HackerRank

---
+++ hackerrank/WarmUp/SimpleArraySum.py
@@ -0,0 +1,42 @@
+"""
+Given an array of  integers, can you find the sum of its elements?
+
+Input Format
+
+The first line contains an integer, , denoting the size of the array.
+The second line contains  space-separated integers representing the array's elements.
+
+Output Format
+
+Print the sum of the array's elements as a single integer.
+
+Sample Input
+
+6
+1 2 3 4 10 11
+Sample Output
+
+31
+Explanation
+
+We print the sum of the array's elements, which is: .
+"""
+
+
+#!/bin/python3
+
+import sys
+
+
+def simpleArraySum(n, ar):
+    # Complete this function
+    sum = 0
+    for i in range(n):
+        sum += ar[i]
+    return sum
+
+
+n = int(input().strip())
+ar = list(map(int, input().strip().split(' ')))
+result = simpleArraySum(n, ar)
+print(result)
