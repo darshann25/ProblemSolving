d050b7cc8ac7df7c874bf199588536ed957fd486
Darshan Patel <darshanpatel25894@gmail.com>
2017-11-15 12:47:12 -0500

    (Implementation) Compare The Triplets

---
+++ hackerrank/Implementation/CompareTheTriplets.py
@@ -0,0 +1,69 @@
+"""
+Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from  to  for three categories: problem clarity, originality, and difficulty.
+
+We define the rating for Alice's challenge to be the triplet , and the rating for Bob's challenge to be the triplet .
+
+Your task is to find their comparison points by comparing  with ,  with , and  with .
+
+If , then Alice is awarded  point.
+If , then Bob is awarded  point.
+If , then neither person receives a point.
+Comparison points is the total points a person earned.
+
+Given  and , can you compare the two challenges and print their respective comparison points?
+
+Input Format
+
+The first line contains  space-separated integers, , , and , describing the respective values in triplet .
+The second line contains  space-separated integers, , , and , describing the respective values in triplet .
+
+Constraints
+
+Output Format
+
+Print two space-separated integers denoting the respective comparison points earned by Alice and Bob.
+
+Sample Input
+
+5 6 7
+3 6 10
+Sample Output
+
+1 1
+Explanation
+
+In this example:
+
+Now, let's compare each individual score:
+
+, so Alice receives  point.
+, so nobody receives a point.
+, so Bob receives  point.
+Alice's comparison score is , and Bob's comparison score is . Thus, we print 1 1 (Alice's comparison score followed by Bob's comparison score) on a single line.
+"""
+
+
+# !/bin/python3
+def solve(a0, a1, a2, b0, b1, b2):
+    # Complete this function
+    aScore = 0
+    bScore = 0
+
+    a = [a0, a1, a2]
+    b = [b0, b1, b2]
+
+    for i in range(len(a)):
+        if a[i] > b[i]:
+            aScore += 1
+        elif a[i] < b[i]:
+            bScore += 1
+
+    return (aScore, bScore)
+
+
+a0, a1, a2 = input().strip().split(' ')
+a0, a1, a2 = [int(a0), int(a1), int(a2)]
+b0, b1, b2 = input().strip().split(' ')
+b0, b1, b2 = [int(b0), int(b1), int(b2)]
+result = solve(a0, a1, a2, b0, b1, b2)
+print(" ".join(map(str, result)))
