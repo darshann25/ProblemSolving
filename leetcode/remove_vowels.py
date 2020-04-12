a1593a8003e1f9daa2c520cb009200bb3cc5eb6d
Darshan Patel <darshanpatel25894@gmail.com>
2020-04-11 22:15:46 -0400

    (Leetcode) Added remove vowels

--- leetcode/remove_vowels.py
+++ leetcode/remove_vowels.py
@@ -1,3 +1,25 @@
+"""
+Given a string S, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
+
+Example 1:
+
+Input: "leetcodeisacommunityforcoders"
+Output: "ltcdscmmntyfrcdrs"
+Example 2:
+
+Input: "aeiou"
+Output: ""
+
+
+Note:
+
+S consists of lowercase English letters only.
+1 <= S.length <= 1000
+"""
+
+# Time Complexity: O(n)
+# Space Complexity: o(1)
+
 class Solution:
     def removeVowels(self, S: str) -> str:
         vowels = 'aeiou'
