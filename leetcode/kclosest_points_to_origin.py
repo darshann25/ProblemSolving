bfbb0e02b37b183a0337e19cbf767440881f0eb2
Darshan Patel <darshanpatel25894@gmail.com>
2020-05-14 14:01:20 -0400

    (Leetcode) Added K closest points to origin problem

---
+++ leetcode/kclosest_points_to_origin.py
@@ -0,0 +1,34 @@
+# Review the following link for the question prompt: https://leetcode.com/problems/find-k-closest-elements/
+
+# O(N + K) time | O(N) space
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        
+        distMemory = {}
+        distList = []
+        
+        for point in points:
+            distPoint = math.sqrt((point[0]**2) + (point[1]**2))
+            
+            if distPoint in distMemory:
+                distMemory[distPoint].append(point)
+            else:
+                distMemory[distPoint] = [point]
+                
+            distList.append(distPoint)
+        
+        distList.sort()
+        distListCounter = 0
+        result = []
+        
+        while K > 0:
+            distListItem = distMemory[distList[distListCounter]]
+            
+            for point in distListItem:
+                if K > 0:
+                    result.append(point)
+                    K -= 1
+            
+            distListCounter += 1
+            
+        return result
