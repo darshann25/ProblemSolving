b69557b8035a36e7618e3d446368dca69f0e9373
Darshan Patel <dpatel96@gatech.edu>
2020-06-02 08:40:10 -0400

    (LeetCode) Updated Reverse Linked List problem

--- leetcode/reverse_linked_list.py
+++ leetcode/reverse_linked_list.py
@@ -1,57 +1,48 @@
-"""
-Reverse a singly linked list.
+# Review the following link for the question prompt: https://leetcode.com/problems/reverse-linked-list/
 
-Example:
-
-Input: 1->2->3->4->5->NULL
-Output: 5->4->3->2->1->NULL
-Follow up:
-
-A linked list can be reversed either iteratively or recursively. Could you implement both?
-"""
+# O(N) time | O(1) space
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-# Time Complexity: 0(n)
-# Space Complexity: 0(1)
 class Solution:
-
-    # Recursive Solution
-    def reverseListRecursive(self, curr):
-
+    
+    def reverseListRecursive(self, curr: ListNode) -> ListNode:
+        
         if curr is None:
             return
-
+        
         if curr.next is None:
             self.head = curr
             return
-
+        
         self.reverseListRecursive(curr.next)
         curr.next.next = curr
         curr.next = None
-
-    def reverseListR(self, head):
-
+            
+    # Recursive Solution
+    def reverseList(self, head: ListNode) -> ListNode:
+        
         if head is None: return head
-
+        
         self.reverseListRecursive(head)
         return self.head
-
-    # Iterative Solution
-    def reverseList(self, head):
-
-        if head is None: return head
-
-        curr = head
-        prev = None
-
-        while curr:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-
-        return prev
+        
+    
+#     # Iterative Solution
+#     def reverseList(self, head: ListNode) -> ListNode:
+        
+#         if head is None: return head
+        
+#         curr = head
+#         prev = None
+        
+#         while curr:
+#             next = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = next
+            
+#         return prev
