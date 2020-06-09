db876621ac6b68c7fa4813bdee28cdb22c82e1af
Darshan Patel <dpatel96@gatech.edu>
2020-06-09 08:26:46 -0400

    (LeetCode) Updated merge intervals

--- leetcode/merge_intervals.py
+++ leetcode/merge_intervals.py
@@ -1,26 +1,26 @@
 # Review the following link for the question prompt: https://leetcode.com/problems/merge-intervals/
 
-# O(n logn + n^2) time | O(n) space
+# O(n logn) time | O(n) space
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        if not intervals: return intervals
+        
         intervals.sort(key = lambda x : x[0])
         resultIntervals = []
-        idx = 0
+        # idx = 0
         
-        while idx < len(intervals):
-            interval = intervals[idx]
-            lowerLimit = interval[0]
-            upperLimit = interval[1]
-            
-            for innerIdx in range(idx+1, len(intervals)):
-                innerInterval = intervals[innerIdx]
-                if innerInterval[0] <= upperLimit:
-                    upperLimit = innerInterval[1] if innerInterval[1] > upperLimit else upperLimit
-                    if idx != len(intervals) - 1: idx += 1
-                else:
-                    break
-            
-            resultIntervals.append([lowerLimit, upperLimit])
-            idx += 1
+        lowerLimit = intervals[0][0]
+        upperLimit = intervals[0][1]
+        
+        for idx in range(1, len(intervals)):
+            if intervals[idx][0] >= lowerLimit and intervals[idx][0] <= upperLimit:
+                upperLimit = max(intervals[idx][1], upperLimit)
+            else:
+                resultIntervals.append([lowerLimit, upperLimit])
+                lowerLimit = intervals[idx][0]
+                upperLimit = intervals[idx][1]
+        
+        resultIntervals.append([lowerLimit, upperLimit])
         
         return resultIntervals
